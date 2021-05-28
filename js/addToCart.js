51c4af5e860244877e9a6fd8e200833942d169da
sebbe1236 <74545889+sebbe1236@users.noreply.github.com>
2021-05-28 15:38:39 +0200

    scuffed js

--- js/addToCart.js
+++ js/addToCart.js
@@ -6,14 +6,19 @@ var cartItems = [
   },
 ];
 const button = document.querySelector(".addtocart-btn");
+let confirmAdded = document.querySelector(".cartinfo-text");
 
 function addToCart(event) {
-  document.querySelector(".cart").style.display = "block";
+  document.querySelector(".cartinfo-text").style.display = "block";
+  document.querySelector(".cartinfo-text").innerHTML += `<p>Product added to cart</p>`;
   let jacketAdded = event.target.dataset.jacket;
+  document.querySelector(".cart").style.display = "block";
+
   document.querySelector(".choosen-items").innerHTML += `
     <ul>${jacketAdded}</ul>
     <ul>Price: 70 euros</ul>
     `;
+
   localStorage.setItem(cartItems, event);
 }
 
