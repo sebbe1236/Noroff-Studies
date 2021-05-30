9e03a3f0ac2ab55a831d38463f1f16d4f161df2d
sebbe1236 <74545889+sebbe1236@users.noreply.github.com>
2021-05-30 15:21:54 +0200

    hotjar removed

--- js/addToCart.js
+++ js/addToCart.js
@@ -1,12 +1,13 @@
 var cartItems = [
   {
-    name: "Skiing jacket",
+    Heading: "You have added",
+    product: "Skiing jacket cross country",
     price: "70 Euros",
-    id: "main",
   },
 ];
 const button = document.querySelector(".addtocart-btn");
 let confirmAdded = document.querySelector(".cartinfo-text");
+let itemList = document.querySelector(".choosen-items");
 
 function addToCart(event) {
   document.querySelector(".cartinfo-text").style.display = "block";
@@ -15,11 +16,29 @@ function addToCart(event) {
   document.querySelector(".cart").style.display = "block";
 
   document.querySelector(".choosen-items").innerHTML += `
-    <ul>${jacketAdded}</ul>
-    <ul>Price: 70 euros</ul>
-    `;
+<ul>${jacketAdded}</ul>
+<ul>Price: 70 euros</ul>
+`;
 
   localStorage.setItem(cartItems, event);
 }
 
 button.addEventListener("click", addToCart);
+
+// function addToCart() {
+//   for (var i = 0; i < cartItems.length; i++) {
+//     if (cartItems < 1) {
+//       button.disabled = false;
+//       itemList.innerHTML += `
+// <p>${cartItems[i].Heading}</p>
+// <p>${cartIteems[i].product}</p>
+// <p>${cartItems[i].price}</p>
+// `;
+//       confirmAdded.innerHTML += ` <p> Product added to cart</p>`;
+//       break;
+//     } else {
+//       button.disabled = true;
+//       document.querySelector(".cartinfo-text").style.display = "block";
+//     }
+//   }
+// }
