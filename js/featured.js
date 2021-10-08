66f4990720f39614439a21591aca357306999144
sebbe1236 <74545889+sebbe1236@users.noreply.github.com>
2021-10-08 14:51:54 +0200

    type error fixed.

--- js/featured.js
+++ js/featured.js
@@ -11,15 +11,13 @@ async function idFetch() {
   try {
     const response = await fetch(Url);
     const details = await response.json();
-    console.log(details);
-    productFetch(details);
+    return details;
   } catch (error) {
     console.log("lol");
   }
 }
-idFetch();
+
 function productFetch(details) {
-  console.log(details);
   queryContent.innerHTML += `
   <h3>${details.name}</h3>
   <div class= "featered-contentpage">
@@ -34,4 +32,6 @@ function productFetch(details) {
     `;
 }
 
-productFetch();
+idFetch().then((result) => {
+  productFetch(result);
+});
